9a8fc2a4d09c8bb7f5ca31d45057a0cdbc6bd2bc
Paurush Rai <paurushrai99@gmail.com>
2024-12-29 22:57:56 +0530

    side nav data file

---
+++ src/data/sideNavRoutes.ts
@@ -0,0 +1,24 @@
+const sideNavRoutes = [
+  {
+    title: 'Tasks',
+    route: '#',
+    icon: 'fluent:calendar-todo-32-light',
+  },
+  {
+    title: 'Documents',
+    route: '#',
+    icon: 'mdi:clipboard-text-history',
+  },
+  {
+    title: 'Team',
+    route: '#',
+    icon: 'ri:team-fill',
+  },
+  {
+    title: 'Settings',
+    route: '#',
+    icon: 'weui:setting-filled',
+  },
+];
+
+export default sideNavRoutes;
