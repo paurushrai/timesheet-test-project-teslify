efb0e127b08627ba7f9d6bb538b25abf054eb132
Paurush Rai <paurushrai99@gmail.com>
2024-12-30 01:48:14 +0530

    added avatar component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="avatar">
+    <img src="/src/assets/avatar.jpeg" alt="" />
+  </button>
+</template>
+
+<style scoped>
+.avatar {
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  border: none;
+  border-radius: 50%;
+  overflow: hidden;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
